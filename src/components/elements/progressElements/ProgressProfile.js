eaa49737992d03673be7e9aaec86c8c147d48865
Ferney021 <eseguragalindo@gmail.com>
2022-06-26 15:14:57 -0500

    Styling to progress page

--- src/components/elements/progressElements/ProgressProfile.js
+++ src/components/elements/progressElements/ProgressProfile.js
@@ -1,51 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const ProgressProfile = () => {
-  return(
+  const [userData, setUserData] = useState({
+    Nombre: 'Ferney Segura',
+    Papa: 3.9,
+    Papi: 4.2,
+    CredTotal: 165,
+    CredCurs: 70,
+    DisOpt: 80,
+    DisObl: 60,
+    FunOpt: 60,
+    FunObl: 30,
+    LibEle: 75,
+  });
+  return (
     <div className="progress-profile-card">
       <div className="progress-profile-ds-top"></div>
       <div className="progress-profile-avatar-holder">
         <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1820405/profile/profile-512.jpg?1533058950" alt="Albert Einstein" />
       </div>
-      <div className="progress-profile-name">
-        <a href="https://codepen.io/AlbertFeynman/" target="_blank">Albert Feynman</a>
-        <h6 title="Followers"><i className="progress-profile-fas progress-profile-fa-users"></i> <span className="progress-profile-followers">90</span></h6>
-      </div>
-      <div className="progress-profile-button">
-        <a href="#" className="progress-profile-btn" onmousedown="follow();">Follow <i className="progress-profile-fas progress-profile-fa-user-plus"></i></a>
-      </div>
-      <div className="progress-profile-ds-info">
-        <div className="progress-profile-ds progress-profile-pens">
-          <h6 title="Number of pens created by the user">Pens <i className="progress-profile-fas progress-profile-fa-edit"></i></h6>
-          <p>29</p>
+      <div className="progress-profile-info">
+        <div className="progress-profile-name">
+          <span>{userData.Nombre}</span>
         </div>
-        <div className="progress-profile-ds progress-profile-projects">
-          <h6 title="Number of projects created by the user">Projects <i className="fas fa-project-diagram"></i></h6>
-          <p>0</p>
-        </div>
-        <div className="progress-profile-ds progress-profile-posts">
-          <h6 title="Number of posts">Posts <i className="progress-profile-fas progress-profile-fa-comments"></i></h6>
-          <p>0</p>
-        </div>
-      </div>
-      <div className="progress-profile-ds-skill">
-        <h6>Skill <i className="progress-profile-fa progress-profile-fa-code" aria-hidden="true"></i></h6>
-        <div className="progress-profile-skill progress-profile-html">
-          <h6><i className="progress-profile-fab progress-profile-fa-html5"></i> HTML5 </h6>
-          <div className="progress-profile-bar progress-profile-bar-html">
-            <p>95%</p>
+        <div className="progress-profile-ds-info">
+          <div className="progress-profile-ds">
+            <h6>P.A.P.A</h6>
+            <p>{userData.Papa}</p>
+          </div>
+          <div className="progress-profile-ds">
+            <h6>P.A.P.I</h6>
+            <p>{userData.Papi}</p>
+          </div>
+          <div className="progress-profile-ds">
+            <h6>Creditos</h6>
+            <p>{userData.CredCurs}/{userData.CredTotal}</p>
           </div>
         </div>
-        <div className="progress-profile-skill progress-profile-css">
-          <h6><i className="progress-profile-fab progress-profile-fa-css3-alt"></i> CSS3 </h6>
-          <div className="progress-profile-bar progress-profile-bar-css">
-            <p>90%</p>
+        <div className="progress-profile-ds-credits">
+          <h5>Avance de creditos</h5>
+          <div className="progress-profile-type">
+            <span className="title">Disciplinar Optativa</span>
+            <div className="progress-profile-bar" style={{ width: `${userData.DisOpt}%` }}></div>
+            <div className="progress-bar-percentage" style={{ width: `${userData.DisOpt}%` }}><div>{userData.DisOpt}%</div></div>
           </div>
-        </div>
-        <div className="progress-profile-skill progress-profile-javascript">
-          <h6><i className="progress-profile-fab progress-profile-fa-js"></i> JavaScript </h6>
-          <div className="progress-profile-bar progress-profile-bar-js">
-            <p>75%</p>
+          <div className="progress-profile-type">
+            <span className="title">Disciplinar Obligatoria</span>
+            <div className="progress-profile-bar" style={{ width: `${userData.DisObl}%` }}></div>
+            <div className="progress-bar-percentage" style={{ width: `${userData.DisObl}%` }}><div>{userData.DisObl}%</div></div>
+          </div>
+          <div className="progress-profile-type">
+            <span className="title">Fundamental Optativa</span>
+            <div className="progress-profile-bar" style={{ width: `${userData.FunOpt}%` }}></div>
+            <div className="progress-bar-percentage" style={{ width: `${userData.FunOpt}%` }}><div>{userData.FunOpt}%</div></div>
+          </div>
+          <div className="progress-profile-type">
+            <span className="title">Fundamental Obligatoria</span>
+            <div className="progress-profile-bar" style={{ width: `${userData.FunObl}%` }}></div>
+            <div className="progress-bar-percentage" style={{ width: `${userData.FunObl}%` }}><div>{userData.FunObl}%</div></div>
+          </div>
+          <div className="progress-profile-type">
+            <span className="title">Libre Eleccion</span>
+            <div className="progress-profile-bar" style={{ width: `${userData.LibEle}%` }}></div>
+            <div className="progress-bar-percentage" style={{ width: `${userData.LibEle}%` }}><div>{userData.LibEle}%</div></div>
           </div>
         </div>
       </div>
--- src/views/Progress.js
+++ src/views/Progress.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProgressProfile from "../components/elements/progressElements/ProgressProfile";
 
 import ProgressSlide from "../components/sections/ProgressSlide";
 
-const Progress = () => {
+import axios from 'axios';
 
+const Progress = () => {
+  useEffect(() => {
+    axios.get('https://7cd7e624-672a-4821-805e-2d2c41e16ab7.mock.pstmn.io/users/academic-histories/4')
+  })
   return (
     <>
-      <h1>
+      {/* <h1>
         Progress
       </h1>
       <ProgressProfile />
-      <ProgressSlide className="illustration-section-02"/>
+    <ProgressSlide className="illustration-section-02"/> */}
+      <div className="progress-container">
+        <h1>Como va tu <span className="text-color-primary">Carrera?</span></h1>
+        <div className="upper-progress-panel">
+          <ProgressProfile />
+          <div>Aqui va un grafico</div>
+        </div>
+      </div>
     </>
   );
 }
