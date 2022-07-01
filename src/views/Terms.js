1517a3b1df94d340164a28dba7bf9746ff2ac40d
Andres <andgonzalezcas@unal.edu.co>
2022-07-01 12:28:34 -0500

    Se completo la seccion de terminos y condiciones

--- src/views/Terms.js
+++ src/views/Terms.js
@@ -16,6 +16,45 @@ const Terms = () => {
           La aplicación cumple con la ley 527 de 1999 que le da validez a un mensaje de texto o correo electrónico como a un documento en físico firmado, por tanto, sin importar el formato se debe conservar el soporte
         </p>
       </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          1-Introduccion:<br/>
+          Las presentes condiciones generales de uso, regulan los términos y condiciones de acceso y uso de (direccion de la pagina), propiedad de (The Graphus Project). El usuario de la aplicacion web deberá de leer y aceptar para hacer uso de todos los servicios e información que se facilitan desde la aplicacion. El acceso y/o utilización de todos o parte de sus contenidos y/o servicios significa la plena aceptación de las presentes condiciones generales de uso. 
+        </p>
+      </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          2-Condiciones de uso:<br/>
+          The graphus cumple con la politica legal por la Universidad Nacional de Colombia para aplicaciones web: "Cuando se ingrese a los sitios web o se utilice un servicio online de la Universidad Nacional de Colombia, se recopilará la información necesaria para brindar acceso, para el funcionamiento del sitio web y para cumplir con los requisitos legales y de seguridad que son necesarios para el funcionamiento del sitio web, o su registro en el mismo, como: número de identificación, email, nombres completos, contraseñas, las direcciones IP y los ajustes del navegador, entre otros. Durante la visita a las plataformas y aplicaciones tecnológicas también se recopilará información acerca de las actividades realizadas en las mismas, para personalizar la experiencia en el sitio web, como, por ejemplo, registrar preferencias y ajustes, y recopilar estadísticas, que permiten a la Universidad mejorar y a seguir desarrollando los sitios web, productos y servicios".  Ver en http://www.legal.unal.edu.co/rlunal/home/doc.jsp?d_i=97992
+        </p>
+      </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          3-Modificaciones:<br/>
+          The Graphus Project se reserva la facultad de modificar en cualquier momento las condiciones generales de uso de la aplicacion web. Esto tambien en respuesta a cambios en la politica legal de la Universidad Nacional de Colombia.
+        </p>
+      </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          4-Obligaciones del usuario:<br/>
+          El usuario deberá respetar en todo momento los términos y condiciones establecidos en las presentes condiciones generales de uso de la aplicacion The Graphus. De forma expresa el usuario manifiesta que utilizará el portal de forma diligente y asumiendo cualquier responsabilidad que pudiera derivarse del incumplimiento de las normas.
+        </p>
+      </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          5-Responsabilidad del sitio web:<br/>
+          El usuario conoce y acepta que la aplicacion web no otorga ninguna garantía de cualquier naturaleza, ya sea expresa o implícita, sobre los datos, contenidos, información y servicios que se incorporan y ofrecen desde la aplicacion The Graphus.
+          Exceptuando los casos que la Ley imponga expresamente lo contrario, y exclusivamente con la medida y extensión en que lo imponga, The Graphus no garantiza ni asume responsabilidad alguna respecto a los posibles daños y perjuicios causados por el uso y utilización de la información, datos y servicios ofrecidos.
+          En todo caso, The Graphus no asume ninguna responsabilidad por los daños y perjuicios que puedan deberse a la información y/o servicios prestados o suministrados por terceros. Toda responsabilidad será del tercero ya sea proveedor o colaborador.
+        </p>
+      </div>
+      <div className="container-xs">
+        <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+          6-Propiedad intelectual:<br/>
+          Todos los contenidos, marcas, logos, dibujos, documentación, programas informáticos o cualquier otro elemento susceptible de protección por la legislación de propiedad intelectual, que sean accesibles en The Graphus corresponden exclusivamente a The Graphus Project o a sus legítimos titulares y quedan expresamente reservados todos los derechos sobre los mismos. Queda expresamente prohibida la creación de enlaces de hipertexto (links) a cualquier elemento integrante de las páginas web de la aplicacion sin la autorización de The Graphus Project, siempre que no sean a una página web de la aplicacion o de la Universidad Nacional de Colombia que no requiera identificación o autenticación para su acceso, o el mismo esté restringido.
+          En cualquier caso, el portal se reserva todos los derechos sobre los contenidos, información, datos y servicios que ostente sobre los mismos. The graphus no concede ninguna licencia o autorización de uso al usuario sobre sus contenidos, datos o servicios, distinta de la que expresamente se detalle en las presentes condiciones generales de uso.
+        </p>
+      </div>
     </section>
   )
 }
